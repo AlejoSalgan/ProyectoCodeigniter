89fabc1e5f70c925268cddec8c7fd4f61bb4ca3c
Alejo <alejosalgan@gmail.com>
2025-01-12 22:46:11 -0300

    Separé las diferentes vistas de la vista principal (Header, Menu (Navbar) y Footer). De esta manera inserto las vistas invocando cada vista sin tener que copiar todo el código en cada vista.

--- application/views/admin/post/list.php
+++ application/views/admin/post/list.php
@@ -16,122 +16,12 @@
         <!-- Site wrapper -->
         <div class="wrapper">
             <!-- Navbar -->
-            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-                <!-- Left navbar links -->
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                    </li>
-                    <li class="nav-item d-none d-sm-inline-block">
-                        <a href="#" class="nav-link"></a>
-                    </li>
-                </ul>
-<!-- cambio-->
-            </nav>
+            <?php $this->load->view("admin/template/header");?>
             <!-- /.navbar -->
-
+            
             <!-- Main Sidebar Container -->
-            <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                <!-- Brand Logo -->
-                <a href="../../index3.html" class="brand-link">
-                    <img src="#" alt="AdminLTE" class="brand-image img-circle elevation-3" style="opacity: .8">
-                    <span class="brand-text font-weight-light">AdminLTE 3</span>
-                </a>
+            <?php $this->load->view("admin/template/nav");?>
 
-                <!-- Sidebar -->
-                <div class="sidebar">
-                    <!-- Sidebar user (optional) -->
-                    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                        <div class="image">
-                            <img src="#" class="img-circle elevation-2" alt="User ">
-                        </div>
-                        <div class="info">
-                            <a href="#" class="d-block">Alexander Pierce</a>
-                        </div>
-                    </div>
-
-                    <!-- Sidebar Menu -->
-                    <nav class="mt-2">
-                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                            <!-- Add icons to the links using the .nav-icon class
-                                 with font-awesome or any other icon font library -->
-                            <li class="nav-item has-treeview">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-tachometer-alt"></i>
-                                    <p>
-                                        Dashboard
-                                        <i class="right fas fa-angle-left"></i>
-                                    </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="../../index.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Dashboard v1</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="../../index2.html" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Dashboard v2</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item has-treeview">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fa fa-list"></i>
-                                    <p>
-                                        Post
-                                        <i class="right fas fa-angle-left"></i>
-                                    </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="<?php echo base_url() ?>admin/post_save" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Crear</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="<?php echo base_url() ?>admin/post_list" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Listar</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item has-treeview">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fa fa-list"></i>
-                                    <p>
-                                        Categorías
-                                        <i class="right fas fa-angle-left"></i>
-                                    </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="<?php echo base_url() ?>admin/category_save" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Crear</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="<?php echo base_url() ?>admin/category_list" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
-                                            <p>Listar</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                    <!-- /.sidebar-menu -->
-                </div>
-                <!-- /.sidebar -->
-            </aside>
 
             <!-- Content Wrapper. Contains page content -->
             <div class="content-wrapper">
@@ -227,8 +117,6 @@
             <!-- /.control-sidebar -->
         </div>
         <!-- ./wrapper -->
-
-
         
         <script src="<?php echo base_url() ?>assets/js/jquery-3.7.1.js"></script>
         
