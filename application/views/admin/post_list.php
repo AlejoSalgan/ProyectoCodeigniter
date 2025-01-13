7f59c317e1d94781a7959fd3af5e21157cf2960e
Alejo <alejosalgan@gmail.com>
2025-01-12 22:25:10 -0300

    Agregando el proyecto principal de Codeigniter (carpeta)

---
+++ application/views/admin/post_list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post_list</title>
+</head>
+<body>
+    <link rel="stylesheet" href="<?php base_url() ?>assets/css/bootstrap.min.css">
+    <script src="<?php base_url()?>assets/js/jquery-3.7.1.js"></script>
+    <script src="<?php base_url()?>assets/js/bootstrap.min.js"></script>
+</body>
+</html>
