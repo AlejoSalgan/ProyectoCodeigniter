dfb6de93fbb2f7d21e9aa5c4217524426d02e02d
Alejo <alejosalgan@gmail.com>
2025-01-15 22:39:04 -0300

    Definimos el template para el controlador 'Admin'.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,19 +3,29 @@
 class Admin extends CI_Controller{
     public function __construct(){
         parent::__construct();
+        $this->load->library("parser");
 
         $this->load->helper("url");
         $this->load->helper("form");
+        $this->load->helper("Post_helper");
     }
     public function index(){
         $this->load->view('admin/test');
     }
 
     public function post_list(){
-        $this->load->view('admin/post/list');
+        //el orden del método parse() tiene que ir al final, sino la vista no recibe el nombre
+        $view["body"] = $this->load->view('admin/post/list', NULL, TRUE);
+        $view["title"] = 'Posts';
+        $this->parser->parse("admin/template/body", $view);
     }
 
     public function post_save(){
-        $this->load->view('admin/post/save');
+        //el orden del método parse() tiene que ir al final, sino la vista no recibe el nombre
+        $data["data_posted"] = posted();
+        $view["body"] = $this->load->view('admin/post/save', $data, TRUE);
+        $view["title"] = 'Crear Post';
+
+        $this->parser->parse("admin/template/body", $view);
     }
 }
--- application/views/admin/post/list.php
+++ application/views/admin/post/list.php
@@ -1,56 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>AdminLTE 3 | Pace</title>
-
-        <!-- Google Font: Source Sans Pro -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="<?php echo base_url() ?>assets/fontawesome-free/css/all.min.css">
-        <!-- adminlte-->
-        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/adminlte.min.css">
-    </head>
-    <body class="hold-transition sidebar-mini pace-primary">
-        <!-- Site wrapper -->
-        <div class="wrapper">
-            <!-- Navbar -->
-            <?php $this->load->view("admin/template/header");?>
-            <!-- /.navbar -->
-            
-            <!-- Main Sidebar Container -->
-            <?php $this->load->view("admin/template/nav");?>
-
-
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <div class="container-fluid">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-
-                            </div>
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="#"></a></li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div><!-- /.container-fluid -->
-                </section>
-
-                <!-- Main content -->
-                <section class="content">
-
-                    <!-- Default box -->
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Bordered Table</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
+<div class="card-body">
                             <table class="table table-bordered">
                                 <thead>
                                     <tr>
@@ -104,25 +52,3 @@
                                 </tbody>
                             </table>
                         </div>
-                        <!-- /.card-body -->
-
-                    </div>
-                    <!-- /.card -->
-
-                </section>
-                <!-- /.content -->
-            </div>
-            <!-- /.content-wrapper -->
-
-            <!-- /.control-sidebar -->
-        </div>
-        <!-- ./wrapper -->
-        
-        <script src="<?php echo base_url() ?>assets/js/jquery-3.7.1.js"></script>
-        
-        <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
-        <!-- AdminLTE App -->
-        <script src="<?php echo base_url() ?>assets/js/admin/adminlte.min.js"></script>
-        
-    </body>
-</html>
